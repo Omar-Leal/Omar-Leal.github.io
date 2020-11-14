db13f4d5ccc0492eb026e6cc4a28c910c098967f
OmarLeal2503 <omarenriquelealr@gmail.com>
2020-11-13 20:35:05 -0400

    MASTER | at last, buttom swicth ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+
 const burgerButtom = document.getElementById('icon-menu');
 const mobileMenu = document.getElementById('links');
 let count = 0;
@@ -6,9 +7,7 @@ burgerButtom.addEventListener('click', function triggerMenu(){
   if(count == 0){
     mobileMenu.classList.add("menu-active");
     mobileMenu.classList.add("movement");    
-    burgerButtom.classList.add("fingerPrint-active");
-    navigator.vibrate(1000);
-
+    burgerButtom.classList.add("fingerPrint-active");    
     count = 1;
   }else {
     mobileMenu.classList.remove("menu-active");
@@ -20,14 +19,101 @@ burgerButtom.addEventListener('click', function triggerMenu(){
       mobileMenu.classList.remove("menu-active");
     }   
 })
+
 // Buttoms experience
+
+//experience templeta
+const buttom1 = () => {
+  const experienceX = `  
+            <li>
+               <span class="spanList"></span>
+                 <div class="jobItem">
+                   <div class="jobItem-title">
+                     <p>April 2017 to present</p>
+                   </div>
+                   <div class="jobItem-content">
+                     <h4>The Centria Group</h4>
+                     <p>SEO Specialist</p>
+                     <p>SEO Manager</p>
+                   </div>
+                 </div>
+             </li>
+             <li>
+               <span class="spanList"></span>
+                 <div class="jobItem">
+                   <div class="jobItem-title">
+                     <p>September 2019 to June 2020</p>
+                   </div>
+                   <div class="jobItem-content">
+                     <h4>iSeen</h4>
+                     <p> Video Editor</p>
+                   </div>
+                 </div>
+             </li>
+             <li>
+               <span class="spanList"></span>
+                 <div class="jobItem">
+                   <div class="jobItem-title">
+                     <p>June 2014 to April 2017</p>
+                   </div>
+                   <div class="jobItem-content">
+                     <h4>i24Web LLC</h4>
+                     <p>SEO Specialist</p>
+                     <p>Marketing Specialist</p>
+                     <p>Traffic Manager</p>
+                     <p>Content Manager</p>
+                   </div>
+                 </div>
+             </li>   
+  
+           `;
+
+           return experienceX;
+
+}
+
+// Education template
+const buttom2 = () => {
+  const educationY = `
+             <li>
+             <span class="spanList"></span>
+               <div class="jobItem">
+                 <div class="jobItem-title">
+                   <p>May 2007 to december 2011</p>
+                 </div>
+                 <div class="jobItem-content">
+                   <h4>Universidad Rafael Belloso Chacín</h4>
+                   <p>SEO Specialist</p>
+                   <p>SEO Manager</p>
+                 </div>
+               </div>
+           </li>
+           `;
+     return educationY;      
+}
+
+let sectCont = document.getElementById("jobsExpe-id");
+
+const text1 = () => {
+   return sectCont.innerHTML = buttom1();
+}
+
+const text2 = () => {
+  return sectCont.innerHTML = buttom2();
+}
+
 let header = document.getElementById("buttoms-id");
 let btns = header.getElementsByClassName("buttom1");
-
+let btn1Content = document.getElementById("jobsExpe-id");
+  text1();
   for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", function() {
       let current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
           this.className += " active";
+
+          
     });
+    
+    
 }
--- src/js/scrolling.js
+++ src/js/scrolling.js
@@ -1,16 +1,15 @@
 
 let scrolling = document.querySelectorAll(".scrolling");
 
-
-
 function showScroll() {
   let scrollTop = document.documentElement.scrollTop;
     for(let i = 0; i < scrolling.length; i++){
       let animateHigh = scrolling[i].offsetTop;
-      if(animateHigh - 800 < scrollTop) {
+      if(animateHigh - 700 < scrollTop) {
         scrolling[i].style.opacity = 1;   
-        scrolling[i].classList.add("showTop")        
-      }        
+        scrolling[i].classList.add("showTop");
+        scrolling[i].classList.add("carrusel-left");           
+      }       
     }
 }
 
