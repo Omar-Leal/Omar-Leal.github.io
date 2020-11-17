f74f02e966a9f1636db198b25db95ac35f19782b
OmarLeal2503 <omarenriquelealr@gmail.com>
2020-11-17 14:57:01 -0400

    MASTER| Animation bar loop adjust

--- src/js/header.js
+++ src/js/header.js
@@ -3,6 +3,7 @@ const deviceSize = window.matchMedia("(min-width: 961px)");
 
   let headerSection = document.getElementById('menu-id');
   let sticky = headerSection.offsetTop;
+  
     function stickyHeader() {
       if(window.pageYOffset > sticky && deviceSize.matches) {
         headerSection.classList.add("stickyHeader");
