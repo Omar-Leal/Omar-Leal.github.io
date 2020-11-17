04c87a7f0d386c684797bcc54da4b2b196232fe1
OmarLeal2503 <omarenriquelealr@gmail.com>
2020-11-17 17:57:32 -0400

    MASTER| texts fixes

--- src/js/scrolling.js
+++ src/js/scrolling.js
@@ -5,11 +5,11 @@ function showScroll() {
   let scrollTop = document.documentElement.scrollTop;
     for(let i = 0; i < scrolling.length; i++){
       let animateHigh = scrolling[i].offsetTop;
-      if(animateHigh - 700 < scrollTop) {
-        scrolling[i].style.opacity = 1;   
-        scrolling[i].classList.add("showTop");
-        scrolling[i].classList.add("carrusel-left");           
-      }       
+      if(animateHigh - 960 < scrollTop) {
+        scrolling[i].style.opacity = 1;
+        scrolling[i].classList.add("showTop");   
+                          
+      }    
     }
 }
 
