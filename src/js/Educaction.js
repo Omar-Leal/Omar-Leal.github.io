b212cc7f36e37e7d4350aa1c43c847bd94ad584c
OmarLeal2503 <omarenriquelealr@gmail.com>
2021-01-06 08:28:22 -0400

    Modules js created and tested

---
+++ src/js/Educaction.js
@@ -0,0 +1,56 @@
+// Education template
+const button2 = () => {
+  const educationY = `
+                    <li>             
+                    <div class="jobItem">
+                      <div class="jobItem-title">
+                        <p>October 2020 to present</p>
+                      </div>
+                      <div class="jobItem-content">
+                        <h4>PLATZI</h4>
+                        <p><a href="https://platzi.com/@omarleal25/curso/1350-html5-css3/diploma/detalle/" target="_blank">The Web Development Course</a></p>
+                        <p><a href="https://platzi.com/@omarleal25/curso/1758-html-practico/diploma/detalle/" target="_blank">HTML and CSS Practical Course</a></p>
+                        <p><a href="https://platzi.com/@omarleal25/curso/1050-programacion-basica/diploma/detalle/" target="_blank">Basic programming course</a></p>
+                        <p><a href="https://platzi.com/@omarleal2503/curso/1814-basico-javascript/diploma/detalle/" target="_blank">Basic JavaScript Course</a></p>
+                        <p><a href="https://platzi.com/p/omarleal2503/curso/1339-fundamentos-javascript/diploma/detalle/" target="_blank">The fundamentals of JavaScript </a></p>
+                        <p><a href="https://platzi.com/p/omarleal2503/curso/1807-scope/diploma/detalle/" target="_blank">Closures and Scope in JavaScript </a></p>
+                        <p><a href="https://platzi.com/@omarleal2503/curso/1748-terminal/diploma/detalle/" target="_blank">Introduction to the Terminal and Command Line Course 2020</a></p>
+                        <p><a href="https://platzi.com/@omarleal2503/curso/1276-terminal-2019/diploma/detalle/" target="_blank">Introduction to the Terminal and Command Line Course 2019</a></p>
+
+
+                        
+                      </div>
+                    </div>
+                  </li>                  
+                   
+                  <li>             
+                  <div class="jobItem">
+                    <div class="jobItem-title">
+                      <p>January 2020</p>
+                    </div>
+                    <div class="jobItem-content">
+                      <h4>UDEMY</h4>
+                      <p><a href="https://www.udemy.com/certificate/UC-7UUJNCYO/" target="_blank">PHP 7 & MySQL: The Complete Course and practical from the beginning</a></p>
+                      
+                    </div>
+                  </div>
+                </li>
+              <li>             
+               <div class="jobItem">
+                 <div class="jobItem-title">
+                   <p>May 2007 to december 2011</p>
+                 </div>
+                 <div class="jobItem-content">
+                   <h4>Rafael Belloso Chacín University</h4>
+                   <p>Marketing Specialist Degree</p>
+                   
+                 </div>
+               </div>
+           </li>
+           `;
+     return educationY;      
+}
+
+export default button2;
+
+
---
+++ src/js/Experience.js
@@ -0,0 +1,51 @@
+const button1 = () => {
+  const experienceX = `  
+            <li>
+               
+                 <div class="jobItem">
+                   <div class="jobItem-title">
+                     <p>April 2017 to present</p>
+                   </div>
+                   <div class="jobItem-content">
+                     <h4>The Centria Group</h4>
+                     <p>SEO Specialist</p>
+                     <p>SEO Manager</p>
+                   </div>
+                 </div>
+             </li>
+             <li>
+               
+                 <div class="jobItem">
+                   <div class="jobItem-title">
+                     <p>September 2019 to June 2020</p>
+                   </div>
+                   <div class="jobItem-content">
+                     <h4>iSeen</h4>
+                     <p> Video Editor</p>
+                   </div>
+                 </div>
+             </li>
+             <li>
+               
+                 <div class="jobItem">
+                   <div class="jobItem-title">
+                     <p>June 2014 to April 2017</p>
+                   </div>
+                   <div class="jobItem-content">
+                     <h4>i24Web LLC</h4>
+                     <p>SEO Specialist</p>
+                     <p>Marketing Specialist</p>
+                     <p>Traffic Manager</p>
+                     <p>Content Manager</p>
+                   </div>
+                 </div>
+             </li>   
+  
+           `;
+
+           return experienceX;
+
+}
+
+export default button1;
+
