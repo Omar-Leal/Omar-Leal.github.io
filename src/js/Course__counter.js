63ba6e2d27d7e3f69253d0baf1d7e370ec2878d6
OmarLeal2503 <omarenriquelealr@gmail.com>
2021-03-11 22:36:24 -0400

    Styling ....

---
+++ src/js/Course__counter.js
@@ -0,0 +1,9 @@
+const display__counter = document.getElementById('counter')
+const get__courses = document.querySelectorAll('.education--courses')
+
+const h3 = document.createElement('h3');
+
+
+
+
+console.log('courses amount ->', get__courses.length);
