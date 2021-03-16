fbd8784f52908c2430492689b744572c23a05013
Omar Leal <omarenriquelealr@gmail.com>
2021-03-16 08:31:56 -0400

    Hot fixes

--- src/js/Course__counter.js
+++ src/js/Course__counter.js
@@ -2,8 +2,5 @@ const display__counter = document.getElementById('counter')
 const get__courses = document.querySelectorAll('.education--courses')
 
 const h3 = document.createElement('h3');
-
-
-
-
 console.log('courses amount ->', get__courses.length);
+
