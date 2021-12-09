8264b80885d260da8453781bdd1c479ffdbbd635
Omar Leal | Front-end Developer <omarenriquelealr@gmail.com>
2021-12-09 09:47:46 -0400

    Some changes

--- src/js/Experience.js
+++ src/js/Experience.js
@@ -1,51 +1,37 @@
-const button1 = () => {
-  const experienceX = `  
-            <li>
-               
-                 <div class="jobItem">
-                   <div class="jobItem-title">
-                     <p>April 2017 to present</p>
-                   </div>
-                   <div class="jobItem-content">
-                     <h4>The Centria Group</h4>
-                     <p>SEO Specialist</p>
-                     <p>SEO Manager</p>
-                   </div>
-                 </div>
-             </li>
-             <li>
-               
-                 <div class="jobItem">
-                   <div class="jobItem-title">
-                     <p>September 2019 to June 2020</p>
-                   </div>
-                   <div class="jobItem-content">
-                     <h4>iSeen</h4>
-                     <p> Video Editor</p>
-                   </div>
-                 </div>
-             </li>
-             <li>
-               
-                 <div class="jobItem">
-                   <div class="jobItem-title">
-                     <p>June 2014 to April 2017</p>
-                   </div>
-                   <div class="jobItem-content">
-                     <h4>i24Web LLC</h4>
-                     <p>SEO Specialist</p>
-                     <p>Marketing Specialist</p>
-                     <p>Traffic Manager</p>
-                     <p>Content Manager</p>
-                   </div>
-                 </div>
-             </li>   
-  
-           `;
+ const jobSection = document.getElementById('jobsExpe__id');
+ const experienceList = [
+   {
+    time: 'April 2021 to Present',
+    company: 'NuOp Inc',
+    jobtitle: 'FrontEnd Developer'
+   },
+   {
+    time: 'April 2017 to February 2021',
+    company: 'The Centria Group',
+    jobtitle: 'SEO Specialist'
+   },
+   {
+    time: 'June 2014 to April 2017',
+    company: 'I24WEB LLC',
+    jobtitle: 'SEO Specialist and Traffic Manager'
+   }
+ ]
 
-           return experienceX;
+experienceList.forEach(job => {
+  jobSection.innerHTML +=  `
+        <li>
+            <div class="jobItem">
+            <div class="jobItem-title">
+              <p>${job.time}</p>
+            </div>
+            <div class="jobItem-content">
+              <h4>${job.company}</h4>
+              <p>${job.jobtitle}</p>
+            </div>
+            </div>       
+        </li>
+  `
+})
 
-}
 
-export default button1;
 
